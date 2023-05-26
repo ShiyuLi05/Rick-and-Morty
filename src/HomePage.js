27ebdc46b33c1c6e3129a2e4384401921388ee7d
李诗语 <lishiyu@shiyus-MacBook-Pro.local>
2023-05-26 11:22:44 -0500

    fix

--- src/HomePage.js
+++ src/HomePage.js
@@ -16,6 +16,7 @@ function HomePage() {
               <Route exact path='/rick-and-morty' element={<Home />} />
               <Route exact path='/' element={<Home />} />
               <Route exact path='/rick-and-morty/character/:character' element={<Character />} />
+              <Route exact path='/character/:character' element={<Character />} />
               <Route exact path='/add' element={<AddForm />} />
               <Route exact path='*' element={<Navigate replace to="/404" />} />
             </Routes>
